33630857f0543967f28507684a6d86ace6eb2343
Nahuel Veron <nahuelav96@gmail.com>
2016-11-24 00:56:33 -0300

    Moved to npm, hail webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,14 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
 const liteServer = require('lite-server');
 
 // Define Variables
-const webpackFile = './webpack.config.js';
-const mainSass = 'src/*.scss';
-const mainJs = 'src/index.js';
-const watch = 'src/**/*';
 const dest = 'dist/';
+const watch = 'src/**/*';
+const mainJs = 'src/index.js';
+const webpackFile = './webpack.config.js';
 
-gulp.task('minify-styles', function() {
-  return gulp.src(mainSass)
-  .pipe(sourcemaps.init())
-  // Pipe Sass Processor
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-
-  // Pipe CSS Autoprefixer
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(sourcemaps.write())
-  // Pipe Output
-  .pipe(gulp.dest(dest));
-});
-
-gulp.task('webpack', function() {
+gulp.task('build', function() {
   return gulp.src(mainJs)
     .pipe(webpack(require(webpackFile)))
     .pipe(gulp.dest(dest));
@@ -40,11 +20,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(watch, ['minify-styles', 'webpack']);
+  gulp.watch(watch, ['build']);
 });
 
 gulp.task('dev', ['build', 'browser-sync', 'watch']);
-
-gulp.task('build', ['minify-styles', 'webpack']);
 
 gulp.task('default', ['build']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,43 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
   devtool: 'inline-source-map',
-  entry: ['babel-polyfill', APP_DIR + '/index.js'],
+  entry: {
+    bundle: [
+      'babel-polyfill',
+      APP_DIR + '/index.scss',
+      APP_DIR + '/index.js'
+    ]
+  },
   output: {
     path: BUILD_DIR,
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
       {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract([
+          'css-loader?-url&sourceMap',
+          'postcss-loader?sourceMap',
+          'sass-loader?sourceMap'
+        ])
+      },
+      {
         test : /\.jsx?/,
         exclude: /(node_modules|bower_components)/,
         include : APP_DIR,
-        loader : 'babel-loader',
-        query: {
-          retainLines: true
-        }
+        loaders : ['babel-loader', 'eslint-loader']
       }
     ]
-  }
+  },
+  eslint: {
+    failOnError: true
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css')
+  ]
 };
